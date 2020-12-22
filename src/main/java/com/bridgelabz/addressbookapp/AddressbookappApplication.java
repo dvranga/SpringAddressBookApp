70d9f0f21681b4bf439a96ba01d1f2b438d04e33
dvranga <dvranga@users.noreply.github.com>
2020-12-23 02:45:45 +0530

    [ranganath]added:Lombok library for logging where logging levels, files and patterns are handled in application.properties

--- src/main/java/com/bridgelabz/addressbookapp/AddressbookappApplication.java
+++ src/main/java/com/bridgelabz/addressbookapp/AddressbookappApplication.java
@@ -2,12 +2,17 @@ package com.bridgelabz.addressbookapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class AddressbookappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AddressbookappApplication.class, args);
+		log.info("Address Book Application started ");
 	}
 
 }
