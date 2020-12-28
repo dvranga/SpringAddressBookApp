726b7e07c2198262e341ed9f9fe00cf7daf6f1c7
dvranga <dvranga@users.noreply.github.com>
2020-12-28 09:14:37 +0530

    [ranganath]Added:Remaining properties to the payroll dto and model

--- src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDTO.java
+++ src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDTO.java
@@ -4,8 +4,10 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 
 import lombok.Data;
+import lombok.ToString;
 
 @Data
+@ToString
 public class AddressBookDTO {
 	
 	@Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$",message = "Name is Invalid!!! ")
@@ -17,6 +19,18 @@ public class AddressBookDTO {
 	@NotEmpty(message = "Address cannot be null")
 	String address;
 
+	@Pattern(regexp ="^[9][1][ ][6-9][0-9]{9}$", message = "PhoneNo is Invalid")
+	@NotEmpty(message = "PhoneNo cannot be null")
+	public String phoneNumber;
+	
+	String city;
+	
+	String state;
+	
+	String zip;
+	
+	
+	
 	public AddressBookDTO(String name, String address) {
 		super();
 		this.name = name;
--- src/main/java/com/bridgelabz/addressbookapp/model/AddressBookData.java
+++ src/main/java/com/bridgelabz/addressbookapp/model/AddressBookData.java
@@ -9,11 +9,18 @@ public class AddressBookData {
 	public int addressBookId;
 	public String name;
 	public String address;
+	public String phoneNumber;
+	public String city;
+	public String state;
+	public String zip;
 	
 	public AddressBookData(int addressBookId, AddressBookDTO addressBookDTO) {
 		this.addressBookId=addressBookId;
 		this.name=addressBookDTO.getName();
 		this.address=addressBookDTO.getAddress();
+		this.phoneNumber=addressBookDTO.getPhoneNumber();
+		this.city=addressBookDTO.getCity();
+		this.state=addressBookDTO.getState();
 	}
 	
 }
